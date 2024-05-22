ee7947e9db572f8ced94359e148e3754c6f14711
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2024-05-22 18:15:11 -0400

    feat(colorshceme): changed to catpuccin

--- lua/config/catppuccin.lua
+++ lua/config/catppuccin.lua
@@ -1,39 +1,34 @@
 local M = {}
 
 function M.setup()
-
- require ("catppuccin").setup({
-   flavour = "mocha",
-   transparent_background = false,
-   backgroud = {
-     light = "latte",
-     dark = "macchiato"
-   },
-   term_colors = true,
-   styles = {
-     functions = {"italic"},
-     keywords = {"bold"},
-   },
-   integrations = {
-     mason = true,
-     nvimtree = true,
-     cmp = true,
-     telescope = true
-   },
-   color_overrides = {
-     latte = {
-       green = "#26851A",
-       flamingo = "#A34F4F",
-       crust = "#e0d39d",
-       rosewater = "#C16651",
-       mantle = "#ffffff",
-       base = "#ffffff",
-       pink = "#67367E",
-       peach = "#D85821",
-       text = "#2d2018"
-     }
-   }
- })
-
+  require("catppuccin").setup({
+    flavour = "macchiato",
+    transparent_background = false,
+    term_colors = true,
+    styles = {
+      functions = { "italic" },
+      keywords = { "bold" },
+    },
+    integrations = {
+      mason = true,
+      nvimtree = true,
+      cmp = true,
+      telescope = true
+    },
+    color_overrides = {
+      latte = {
+        green = "#26851A",
+        flamingo = "#A34F4F",
+        crust = "#e0d39d",
+        rosewater = "#C16651",
+        mantle = "#ffffff",
+        base = "#ffffff",
+        pink = "#67367E",
+        peach = "#D85821",
+        text = "#2d2018"
+      }
+    }
+  })
 end
+
 return M
