dae85e063e2007675c3ce44acd17da30aa93eea3
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2024-08-05 17:10:00 -0400

    feat: refactor

--- lua/config/dap-python.lua
+++ lua/config/dap-python.lua
@@ -1,27 +1,27 @@
-local dap = require("dap")
+local ny_dap = require("dap")
 local M = {}
 function M.setup()
-  require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
-  require('dap-python').resolve_python = function()
-    return '/home/gonzalo/dev/bpk-training/training-service/venv/bin/python'
-  end
+	require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
+	require("dap-python").resolve_python = function()
+		return "/home/gonzalo/dev/bpk-training/training-service/venv/bin/python"
+	end
 
-  table.insert(dap.configurations.python, {
-    type = "python",
-    request = "launch",
-    name = "Module App",
-    console = "integratedTerminal",
-    module = "app", -- edit this to be your app's main module
-    cwd = "${workspaceFolder}",
-  })
-  table.insert(dap.configurations.python, {
-    type = "python",
-    request = "launch",
-    name = "Module main",
-    console = "integratedTerminal",
-    module = "main", -- edit this to be your app's main module
-    cwd = "${workspaceFolder}",
-  })
+	table.insert(ny_dap.configurations.python, {
+		type = "python",
+		request = "launch",
+		name = "Module App",
+		console = "integratedTerminal",
+		module = "app", -- edit this to be your app's main module
+		cwd = "${workspaceFolder}",
+	})
+	table.insert(ny_dap.configurations.python, {
+		type = "python",
+		request = "launch",
+		name = "Module main",
+		console = "integratedTerminal",
+		module = "main", -- edit this to be your app's main module
+		cwd = "${workspaceFolder}",
+	})
 end
 
 return M
