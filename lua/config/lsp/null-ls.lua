df3bf983106b075dd8a2647ab5172187176f3707
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2024-10-07 15:42:56 -0400

    fix(null-ls): Fixed styling

--- lua/config/lsp/null-ls.lua
+++ lua/config/lsp/null-ls.lua
@@ -15,7 +15,11 @@ local async = event == "BufWritePost"
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier,
+		formatting.prettier.with({
+			condition = function(utils)
+				return utils.root_has_file({ ".prettierrc" })
+			end,
+		}),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
 		diagnostics.flake8,
@@ -27,17 +31,6 @@ null_ls.setup({
 			vim.keymap.set("n", "<Leader>f", function()
 				vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
 			end, { buffer = bufnr, desc = "[lsp] format" })
-
-			-- format on save
-			--vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-			--vim.api.nvim_create_autocmd(event, {
-			--	buffer = bufnr,
-			--	group = group,
-			--	callback = function()
-			--		vim.lsp.buf.format({ bufnr = bufnr, async = async })
-			--	end,
-			--	desc = "[lsp] format on save",
-			--})
 		end
 	end,
 })
