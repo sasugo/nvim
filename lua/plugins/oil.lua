23dd9f13a68792e1e3f53f6e314525f18b54331d
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2024-11-04 18:50:32 -0400

    feat(lazy): added lualine and oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,57 @@
+return {
+  "stevearc/oil.nvim" ,
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("oil").setup({
+      columns = {
+      	"icon",
+      	"permissions",
+      	"size",
+      	"mtime",
+      },
+      delete_to_trash = false,
+      view_options = {
+      	show_hidden = true,
+      },
+      keymaps = {
+      	["g?"] = "actions.show_help",
+      	["<CR>"] = "actions.select",
+      	["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+      	["<C-h>"] = {
+      		"actions.select",
+      		opts = { horizontal = true },
+      		desc = "Open the entry in a horizontal split",
+      	},
+      	["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+      	["<C-p>"] = "actions.preview",
+      	["<C-c>"] = "actions.close",
+      	["<C-l>"] = "actions.refresh",
+      	["-"] = "actions.parent",
+      	["_"] = "actions.open_cwd",
+      	["`"] = "actions.cd",
+      	["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+      	["gs"] = "actions.change_sort",
+      	["gx"] = "actions.open_external",
+      	["g."] = "actions.toggle_hidden",
+      	["gd"] = {
+      		desc = "Toggle file detail view",
+      		callback = function()
+      			detail = not detail
+      			if detail then
+      				require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+      			else
+      				require("oil").set_columns({ "icon" })
+      			end
+      		end,
+      	},
+      	["g\\"] = "actions.toggle_trash",
+      },
+      use_default_keymaps = true,
+    })
+  end,
+  keys = {
+    {"<leader>-", "<cmd>Oil --float<CR>"}
+  }
+}
+
