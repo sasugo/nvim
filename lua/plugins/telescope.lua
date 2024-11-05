5851b474a00f795ccff0ed441b764b018c9ecd42
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2024-11-05 09:05:22 -0400

    feat(lazy): installed telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,53 @@
+return {
+  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  dependencies = { 
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope-live-grep-args.nvim' 
+   },
+  config = function()
+    local telescope = require("telescope")
+    local lga_actions = require("telescope-live-grep-args.actions")
+    
+    telescope.setup {
+      defaults = {
+        default_ignore_patterns = {
+          "node_modules", "vendor"
+        }
+      },
+      pickers = {
+        find_files = {
+          hidden = true
+        }
+      },
+      extensions = {
+        live_grep_args = {
+          auto_quoting = true, -- enable/disable auto-quoting
+          -- define mappings, e.g.
+          mappings = {         -- extend mappings
+            i = {
+              ["<C-k>"] = lga_actions.quote_prompt(),
+              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+              -- freeze the current list and start a fuzzy search in the frozen list
+            },
+          },
+          -- ... also accepts theme settings, for example:
+          -- theme = "dropdown", -- use dropdown theme
+          -- theme = { }, -- use own theme spec
+          -- layout_config = { mirror=true }, -- mirror preview pane
+        }
+      }
+    }
+    
+    -- don't forget to load the extension
+    telescope.load_extension("live_grep_args")
+
+    -- Keymaps
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    --vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    vim.keymap.set('x', '<leader>ss', '"zy<Cmd>Telescope live_grep<CR><C-r>z', {})
+  end
+}
