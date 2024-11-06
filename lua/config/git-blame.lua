7162462afed7d36daa678c55ef9890e520596662
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2024-11-06 00:18:15 -0400

    feat(lazy): more plugins installed

--- lua/config/git-blame.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-function M.setup()
-  require("gitblame").setup({
-    enabled = true
-  })
-end
-
-return M
--- lua/config/navic.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-function M.setup()
-  require("nvim-navic").setup({
-    auto_attach = true
-  })
-end
-
-return M
