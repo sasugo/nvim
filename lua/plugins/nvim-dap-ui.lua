c9b9df9e0edca6dfabd060cf9fbb01ff1c6636ec
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2024-11-07 17:22:18 -0400

    feat(lazy): finally finished

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -38,6 +38,59 @@ return {
         end,
       },
     }
+
+    local cwd = vim.fn.getcwd()
+    local dap_python = require("dap-python")
+    dap_python.setup("~/.virtualenvs/debugpy/bin/python")
+
+    table.insert(dap.configurations.python, {
+      type = "python",
+      request = "launch",
+      name = "Module App",
+      cwd = cwd,
+      module = "app",
+      pythonPath = function()
+        if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+          return cwd .. "/venv/bin/python"
+        elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+          return cwd .. "/.venv/bin/python"
+        else
+          return "/usr/bin/python"
+        end
+      end,
+    })
+
+    table.insert(dap.configurations.python, {
+      type = "python",
+      name = "BPKT - Remote User Service",
+      request = "attach",
+      mode = "remote",
+      port = 10191,
+      host = "localhost",
+      cwd = cwd,
+      pathMappings = {
+        {
+          localRoot = cwd,
+          remoteRoot = "/app",
+        },
+      },
+    })
+
+    table.insert(dap.configurations.python, {
+      type = "python",
+      name = "BPKT - Remote Training Service",
+      request = "attach",
+      mode = "remote",
+      port = 10192,
+      host = "localhost",
+      cwd = cwd,
+      pathMappings = {
+        {
+          localRoot = cwd,
+          remoteRoot = "/app",
+        },
+      },
+    })
     vim.keymap.set("n", "<leader><F3>", dapui.toggle, {})
     vim.keymap.set("n", "<leader><F1>", dapui.open, {})
     dapui.setup()
