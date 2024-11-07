c228f6866db71286697c267828f8045288e94d4f
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2024-11-06 23:52:40 -0400

    feat(lazy): fixed theme

---
+++ lua/plugins/alabaster.lua
@@ -0,0 +1,6 @@
+return {
+  "p00f/alabaster.nvim",
+  config = function()
+    vim.cmd.colorscheme("alabaster")
+  end,
+}
--- lua/plugins/patana.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "cvigilv/patana.nvim",
-  config = function()
-    vim.opt.background = "light"
-    vim.cmd([[colorscheme patana]])
-  end,
-}
