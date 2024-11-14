81881e4266e783dbcc30bd5741190085e2a2f826
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2024-11-14 15:18:58 -0400

    feat(luasnip): fixed html and css snippets

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,10 +1,10 @@
 return {
   "L3MON4D3/Luasnip",
+  version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+  -- install jsregexp (optional!).
+  build = "make install_jsregexp",
+  dependencies = { "rafamadriz/friendly-snippets" },
   config = function()
-    local luasnip = require("luasnip")
-    luasnip.config.set_config({
-      history = false,
-      updateevents = "TextChanged,TextChangedI",
-    })
+    require("luasnip.loaders.from_vscode").lazy_load()
   end,
 }
