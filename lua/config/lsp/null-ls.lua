92df6bb45414e11aa669009f48db775756111d71
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2025-02-18 16:34:33 -0400

    feat(null-ls): changes to black

--- lua/config/lsp/null-ls.lua
+++ lua/config/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+  return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -9,27 +9,27 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		formatting.prettier.with({
-			condition = function(utils)
-				return utils.root_has_file({ ".prettierrc" })
-			end,
-		}),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
-		diagnostics.flake8,
-		diagnostics.eslint_d.with({
-			filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "html", "spec.ts" },
-		}),
-		formatting.phpcbf,
-		formatting.csharpier,
-	},
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.keymap.set("n", "<Leader>f", function()
-				vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-			end, { buffer = bufnr, desc = "[lsp] format" })
-		end
-	end,
+  debug = false,
+  sources = {
+    formatting.prettier.with({
+      condition = function(utils)
+        return utils.root_has_file({ ".prettierrc" })
+      end,
+    }),
+    formatting.black,
+    formatting.stylua,
+    diagnostics.flake8,
+    diagnostics.eslint_d.with({
+      filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "html", "spec.ts" },
+    }),
+    formatting.phpcbf,
+    formatting.csharpier,
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.keymap.set("n", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+    end
+  end,
 })
