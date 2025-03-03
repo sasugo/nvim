68b3934d84e4c7368f798c452a42e4a72cec891c
Er. Gonzalo Unzueta <gunzueta@siwel.com.bo>
2025-03-03 13:36:32 -0400

    feat(moonfly): config

--- lua/plugins/moonfly.lua
+++ lua/plugins/moonfly.lua
@@ -4,7 +4,10 @@ return {
   lazy = false,
   priority = 1000,
   init = function()
+    -- Lua initialization file
+    vim.g.moonflyTransparent = true
+    -- Lua cursor
+    vim.g.moonflyCursorColor = true
     vim.cmd([[colorscheme moonfly]])
   end
 }
-
