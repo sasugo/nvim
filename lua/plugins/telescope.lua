3ccd7998f1b79f03987fd29fe770da7dc7664307
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2025-04-02 10:22:28 -0400

    feat(telescope): updated layout for buffers and find files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,12 +10,6 @@ return {
     local lga_actions = require("telescope-live-grep-args.actions")
 
     telescope.setup({
-      buffers = {
-        sort_mru = true,
-        ignore_current_buffer = true, -- Optional: skips the current buffer in the list
-        previewer = false,          -- Optional: disables preview for faster loading
-        layout_config = { width = 80 }, -- Optional: adjust width for readability
-      },
       defaults = {
         default_ignore_patterns = {
           "node_modules",
@@ -23,36 +17,45 @@ return {
         },
       },
       pickers = {
+        buffers = {  -- Moved buffers config here under pickers
+          sort_mru = true,
+          ignore_current_buffer = true,
+          previewer = false,
+          layout_config = {
+            width = 0.8,
+            height = 0.6,
+            prompt_position = "top",
+            preview_cutoff = 120,
+          },
+        },
         find_files = {
           hidden = true,
+          previewer = false,
+          layout_config = {
+            width = 0.8,
+            height = 0.6,
+            prompt_position = "top",
+            preview_cutoff = 120,
+          },
         },
       },
       extensions = {
         live_grep_args = {
-          auto_quoting = true, -- enable/disable auto-quoting
-          -- define mappings, e.g.
-          mappings = {    -- extend mappings
+          auto_quoting = true,
+          mappings = {
             i = {
               ["<C-k>"] = lga_actions.quote_prompt(),
               ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-              -- freeze the current list and start a fuzzy search in the frozen list
             },
           },
-          -- ... also accepts theme settings, for example:
-          -- theme = "dropdown", -- use dropdown theme
-          -- theme = { }, -- use own theme spec
-          -- layout_config = { mirror=true }, -- mirror preview pane
         },
       },
     })
 
-    -- don't forget to load the extension
     telescope.load_extension("live_grep_args")
 
-    -- Keymaps
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-    --vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
     vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
