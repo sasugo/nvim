57f30fdbf41b062e7cdd8df2354a11e4afdfb230
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2025-04-02 18:04:22 -0400

    fix(lsp): fixed on_attach handler

--- lua/config/lsp/handlers.lua
+++ lua/config/lsp/handlers.lua
@@ -97,9 +97,6 @@ M.on_attach = function(client, bufnr)
     navic.attach(client, bufnr)
     navbuddy.attach(client, bufnr)
   end
-  vim.diagnostic.config({
-    update_in_insert = true,
-  }, bufnr) -- Apply to current buffer
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
