655ddd8de2696e26684ae84f2310a7452e453f34
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2025-04-08 17:29:29 -0400

    feat(none-ls): fixed formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,22 +3,34 @@ return {
   dependencies = { "nvimtools/none-ls-extras.nvim" },
   lazy = false,
   config = function()
-    local none_ls_status_ok, none_ls = pcall(require, "none-ls")
-    if not none_ls_status_ok then
+    local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+    if not null_ls_status_ok then
       return
     end
     local augroup = vim.api.nvim_create_augroup("Format", { clear = true })
 
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-    local formatting = none_ls.builtins.formatting
+    local formatting = null_ls.builtins.formatting
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-
-    none_ls.setup({
+    null_ls.setup({
       debug = false,
       sources = {
         formatting.prettier.with({
           condition = function(utils)
-            return utils.root_has_file({ ".prettierrc" })
+            -- Check for .prettierrc or .prettierrc.json
+            local has_prettierrc = utils.root_has_file({ ".prettierrc", ".prettierrc.json" })
+            -- Check for package.json with a "prettier" field
+            local has_package_json_with_prettier = false
+            if utils.root_has_file({ "package.json" }) then
+              local package_json_path = vim.fn.findfile("package.json", vim.fn.getcwd() .. ";")
+              if package_json_path ~= "" then
+                local package_json = vim.fn.json_decode(vim.fn.readfile(package_json_path))
+                has_package_json_with_prettier = package_json and package_json.prettier ~= nil
+              end
+            end
+
+            -- Return true if either condition is met
+            return has_prettierrc or has_package_json_with_prettier
           end,
         }),
         formatting.black.with({ extra_args = { "--fast" } }),
@@ -35,13 +47,14 @@ return {
             "spec.ts",
           },
           condition = function(utils)
-            return utils.root_has_file({
+            local has_eslint_config = utils.root_has_file({
               ".eslintrc.js",
               ".eslintrc.json",
               ".eslintrc.cjs",
               "eslint.config.js",
               "eslint.config.mjs",
             })
+            return has_eslint_config
           end,
         }),
         formatting.phpcbf,
