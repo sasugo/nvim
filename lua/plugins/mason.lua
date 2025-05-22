25d09a393e9ab28a5f57b4f214a6ae07a74564b3
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2025-05-22 10:23:02 -0400

    pin mason and mason-ls config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,8 +1,9 @@
 return {
-  { "williamboman/mason.nvim", opts = {} },
+  { "williamboman/mason.nvim", opts = {}, tag = "v1.11.0" },
 
   {
     "williamboman/mason-lspconfig.nvim",
+    tag = "v1.32.0",
     dependencies = { { "neovim/nvim-lspconfig" }, "SmiteshP/nvim-navic" },
     opts = {},
   },
