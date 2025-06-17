10bab990bc53e04ad9942509875b3de459b0c981
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2025-06-17 15:31:15 -0400

    feat(cursor): prettier

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -1,4 +1,14 @@
 return {
   "sphamba/smear-cursor.nvim",
-  opts = {},
+  opts = {
+    smear_between_buffers = true,
+    smear_insert_mode = true,
+    cursor_color = "#D4A017",
+    stiffness = 0.3,
+    trailing_stiffness = 0.1,
+    trailing_exponent = 5,
+    never_draw_over_target = true,
+    hide_target_hack = true,
+    gamma = 1,
+  },
 }
