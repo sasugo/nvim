43c6b82bf36dd4d8a061d55470307062caec90ac
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2025-06-23 19:40:39 -0400

    feat(smear-cursor): removed

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -1,11 +1,12 @@
 return {
   "sphamba/smear-cursor.nvim",
+  enabled = false,
   opts = {
     smear_between_buffers = true,
     smear_insert_mode = true,
     cursor_color = "#D4A017",
-    stiffness = 0.3,
-    trailing_stiffness = 0.1,
+    stiffness = 0.9,
+    trailing_stiffness = 0.9,
     trailing_exponent = 5,
     never_draw_over_target = true,
     hide_target_hack = true,
