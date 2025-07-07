f44fec270b8ae5cf7f540f683c581079fd97d853
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2025-07-07 09:04:30 -0400

    feat(telescope): using theme now

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -56,7 +56,9 @@ return {
     telescope.load_extension("live_grep_args")
 
     local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+    vim.keymap.set("n", "<leader>ff", function()
+      builtin.find_files(require("telescope.themes").get_dropdown({}))
+    end, {})
     vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
     vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
