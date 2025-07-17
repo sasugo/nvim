79dc716977dec446955d7711a7f52f626333a26a
Er. Gonzalo Unzueta <gunzueta@siwel.com.bo>
2025-07-16 22:51:32 -0400

    feat(kanagawa): changed tranparency

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -2,6 +2,7 @@ return {
   "rebelot/kanagawa.nvim",
   config = function()
     require("kanagawa").setup({
+      transparent = true,
       colors = {
         theme = {
           all = {
@@ -116,8 +117,8 @@ return {
           -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
           NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
 
-          -- Popular plugins that open floats will link to NormalFloat by default;
-          -- set their background accordingly if you wish to keep them dark and borderless
+          -- -- Popular plugins that open floats will link to NormalFloat by default;
+          -- -- set their background accordingly if you wish to keep them dark and borderless
           LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
           MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
         }
