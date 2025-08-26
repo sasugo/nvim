261cb3cbfb69f87fe80b4cde836ff7dade8b4093
Er. Gonzalo Unzueta <gonzalo.unzueta@bupunku.com>
2025-08-25 22:10:51 -0400

    feat(lazygit): added colors

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -19,6 +19,10 @@ return {
 		vim.g.lazygit_floating_window_scaling_factor = 0.8 -- 80% of screen size
 		vim.g.lazygit_floating_window_border_chars = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" } -- Rounded border
 		vim.g.lazygit_floating_window_use_plenary = 1 -- Use plenary.nvim for floating window
+		-- Set custom highlight groups for LazyGit floating window
+		vim.api.nvim_set_hl(0, "LazyGitFloat", { bg = "#eeeeee", fg = "#4d4d4c" })
+		vim.api.nvim_set_hl(0, "LazyGitBorder", { fg = "#4271ae" })
+		vim.api.nvim_set_hl(0, "LazyGitCursor", { fg = "#aaaaaa", bg = "#f5f5f5" })
 	end,
 	-- setting the keybinding for LazyGit with 'keys' is recommended in
 	-- order to load the plugin when the command is run for the first time
